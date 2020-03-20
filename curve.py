20fb4384d7261dbe9a87a7c883ed16a11778b1d0
abuslang <asquadri1@gmail.com>
2020-03-20 00:43:46 -0500

    working 2nd order

--- curve.py
+++ curve.py
@@ -15,19 +15,42 @@ gameDisplay = pygame.display.set_mode((512,512))
 gameDisplay.fill(black)
 
 
-
-def Hilbert(i):
-    points = [[0,0], [0,1], [1,1], [1,0]]
-    return points[i]
-
-order = 1
+order = 2
 n = math.pow(2, order)
 total = n * n           # total number of points
 total = int(total)
 
-path = [[0,0] for i in range(int(total))]
 
-for i in range(0,int(total)):
+def Hilbert(i):
+    points = [[0,0], [0,1], [1,1], [1,0]]
+    index = i & 3
+    returnPoint = points[index]
+
+    i = i >> 2
+    index = i & 3
+
+    if index == 0:
+        temp = returnPoint[0]
+        returnPoint[0] = returnPoint [1]
+        returnPoint[1] = temp
+    elif index == 1:
+        returnPoint[1] += order
+    elif index == 2:
+        returnPoint[0] += order
+        returnPoint[1] += order
+    elif index == 3:
+        temp = 1 - returnPoint[0]
+        returnPoint[0] = 1 - returnPoint[1]
+        returnPoint[1] = temp
+        returnPoint[0] += order
+
+    return returnPoint
+
+
+
+path = [[0,0] for i in range(total)]
+
+for i in range(0,total):
     path[i] = Hilbert(i)
     len = 512 / n
     path[i][0] *= len
